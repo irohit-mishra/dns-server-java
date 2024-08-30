71fa89f62bb1906a73300496fb2bcd94032c3ee3
Rohit Mishra <iamrohit330@gmail.com>
2024-08-30 11:19:43 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,44 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.BitSet;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    try(DatagramSocket serverSocket = new DatagramSocket(2053)) {
-      while(true) {
-         final byte[] buf = new byte[512];
-         final DatagramPacket packet = new DatagramPacket(buf, buf.length);
-         serverSocket.receive(packet);
-         System.out.println("Received data");
-    
-         final byte[] bufResponse = new byte[512];
-         final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
-         serverSocket.send(packetResponse);
-       }
-     } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-     }
-  }
+        // Uncomment this block to pass the first stage
+        try (DatagramSocket serverSocket = new DatagramSocket(2053)) {
+            while (true) {
+                final byte[] buf = new byte[512];
+                final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                serverSocket.receive(packet);
+                System.out.println("Received data");
+
+                // Prepare response buffer
+                short ID = (short) 1234;
+                final var bitset = new BitSet(8);
+                bitset.flip(7);
+
+                final byte[] bufResponse = ByteBuffer.allocate(512)
+                        .order(ByteOrder.BIG_ENDIAN)
+                        .putShort(ID)
+                        .put(bitset.toByteArray())
+                        .put((byte) 0)
+                        .putShort((short) 0)
+                        .putShort((short) 0)
+                        .putShort((short) 0)
+                        .putShort((short) 0)
+                        .array();
+
+                final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
+                serverSocket.send(packetResponse);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
